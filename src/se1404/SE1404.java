0c6682d083207f606e58a467598377e1f67ed1b2
votantai <votantai4899@gmail.com>
2020-04-14 11:50:46 +0700

    first commit

---
+++ src/se1404/SE1404.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se1404;
+
+/**
+ *
+ * @author votan
+ */
+public class SE1404 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("5! = " + MyToys.cF(5));
+        System.out.println("6! = " + MyToys.cF(6));
+        System.out.println("0! = " + MyToys.cF(0));
+        System.out.println("-5! = " + MyToys.cF(-5));
+    }
+}
